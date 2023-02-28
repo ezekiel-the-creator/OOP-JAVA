ab639abf6e1f5a497012d49e2ea1ccb51b162a32
Zak-Rey <Zaktripple@gmail.com>
2023-02-28 08:22:13 +0300

    updating person, added setter and getter methods for name and age variables

--- Person.java
+++ Person.java
@@ -1,7 +1,7 @@
 public class Person {
     
     //Declaring private member variables
-    privat String name;
+    private String name;
     private int age;
     private String address;
 
@@ -10,12 +10,22 @@ public class Person {
         return name;
     }
 
+    //Define setter method for name variable
+    public void setName(String name){
+        this.name = name;
+    }
+
+    //getter method for "age" variable
+    public int getAge(){
+        return age;
+    } 
+
     //setter method for 'age' variable
     public void setAge(int age) {
         this.age = age;
     }
     
-    //getter method for 'address variable'
+    //getter method for 'address' variable
     public String getAdress() {
         return address;
     }
@@ -30,6 +40,6 @@ public class Person {
     public void printDetails() {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
-        System.out.println("Adress: " address)
+        System.out.println("Adress: " + address);
     }
 }
