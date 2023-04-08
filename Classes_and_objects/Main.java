ab76b09cab158252006ad6226682c5730ddde9dd
Zak-Rey <zaktripple@gmail.com>
2023-04-08 18:21:02 +0300

    class and onjects

---
+++ Classes_and_objects/Main.java
@@ -0,0 +1,11 @@
+package Classes_and_objects;
+
+public class Main {
+    public static void main(String[] args) {
+        Lamp led = new Lamp();
+        Lamp halolgen = new Lamp();   
+
+        led.turnOn();
+        halolgen.turnOff();
+    }
+}
