d33669690c28a3a70bde963fffeb5c466e4a9120
Zak-Rey <zaktripple@gmail.com>
2023-04-08 18:37:45 +0300

    Method overloading

--- Method_overloading/Main.java
+++ Method_overloading/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Creating an object an calling overloaded methods");
         HelperService hs = new HelperService();
-        System.out.println(hs.formatNumber(500));
-        System.out.println(hs.formatNumber(89.9934));
-        System.out.println(hs.formatNumber("550"));
+        System.out.println(hs.formatNumber(500));//parameter type int
+        System.out.println(hs.formatNumber(89.9934));//parameter type double
+        System.out.println(hs.formatNumber("550"));//parameter type String
     }
 }
