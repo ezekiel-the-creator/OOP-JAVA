d4d0d1e44d08973ab88c03f11bb25e43cac8af37
Zak-Rey <zaktripple@gmail.com>
2023-04-08 18:33:28 +0300

    class and onjects

---
+++ Method_overloading/Java Method Overloading.java
@@ -0,0 +1,26 @@
+package Method_overloading;
+//Method Overloading - two or more functions have the same name but different parameters(different number of parameters, different types of parameters, or both).
+
+//EXAMPLE:
+
+//    void func() {...}
+//    void func(int a) {...}
+//    void func(double a) {...}
+//    void func(double a, float b) {.....}
+   
+//Example code:
+
+class HelperService {
+    public String formatNumber (int value) {
+        return String.format("%d", value);
+    }
+    
+    private String formatNumber (double value){
+        return String.format("%.3f", value);
+    }
+    
+    private String formatNumber(String value){
+        return String.format("%.2f", Double.parseDouble(value));
+    }
+}
+
