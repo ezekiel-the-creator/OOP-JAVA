589f02532b07a5d1c43fa1e7f88120d876226a06
Zak-Rey <zaktripple@gmail.com>
2023-04-08 18:59:36 +0300

    java constructors

---
+++ Java_Constructors/ParameterizedConstructor.java
@@ -0,0 +1,9 @@
+package Java_Constructors;
+
+public class ParameterizedConstructor {
+    String languages;
+
+    ParameterizedConstructor(String language){
+        languages = language;
+    }
+}
