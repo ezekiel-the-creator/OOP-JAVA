69620a1284b677ab1a04eb5e0a5e1449feeeacfe
Zak-Rey <zaktripple@gmail.com>
2023-04-08 18:17:54 +0300

    class and onjects

--- Classes_and_objects/Lamp.java
+++ Classes_and_objects/Lamp.java
@@ -1,5 +1,15 @@
 package Classes_and_objects;
 
 public class Lamp {
-    
+    boolean isOn;
+
+    public void turnOn(){
+        isOn = true;
+        System.out.println("Light on?" + isOn);
+    }
+
+    public void turnOff(){
+        isOn = false;
+        System.out.println("Light on?" + isOn);
+    }
 }
