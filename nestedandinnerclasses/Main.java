764defac1ec4d86b5ab88a46d088c1ead9ace667
Zak-Rey <zaktripple@gmail.com>
2023-04-12 20:39:46 +0300

    Inner and Nested class

--- nestedandinnerclasses/Main.java
+++ nestedandinnerclasses/Main.java
@@ -2,12 +2,16 @@ package nestedandinnerclasses;
 
 public class Main {
     public static void main(String... args){
-        //Inner class
+        //Inner classes CPU and Processor
         CPU cpu = new CPU();
         CPU.Processor p1 = cpu.new Processor();
         CPU.RAM ram = cpu.new RAM();
 
         System.out.println("Processor Cache = " + p1.getCache());
         System.out.println("Ram Clock Speed = " + ram.getClockspeed());
+
+        //Nested class USB
+        MotherBoard.USB usb = new MotherBoard.USB();
+        System.out.println("Total Ports = " + usb.getTotalPorts());
     }
 }
