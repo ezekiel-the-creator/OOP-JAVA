677b6ba56bdb96d9877c53f5b911268e5c02da5b
Zak-Rey <zaktripple@gmail.com>
2023-04-12 20:40:19 +0300

    Nested Class

---
+++ nestedandinnerclasses/MotherBoard.java
@@ -0,0 +1,12 @@
+package nestedandinnerclasses;
+
+public class MotherBoard {
+    static class USB{
+        int usb2 = 2;
+        int usb3 = 1;
+
+        int getTotalPorts() {
+            return usb2 + usb3;
+        }
+    }
+}
