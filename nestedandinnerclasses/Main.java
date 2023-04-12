0b2e04e9f1466658d84946d776d71d996004af0d
Zak-Rey <zaktripple@gmail.com>
2023-04-12 20:29:12 +0300

    Inner class

--- nestedandinnerclasses/Main.java
+++ nestedandinnerclasses/Main.java
@@ -3,5 +3,9 @@ package nestedandinnerclasses;
 public class Main {
     public static void main(String... args){
         CPU cpu = new CPU();
+        CPU.Processor p1 = cpu.new Processor();
+        CPU.RAM ram = cpu.new RAM();
+
+        System.out.println("Process");
     }
 }
