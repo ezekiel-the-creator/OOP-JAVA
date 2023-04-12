07a3765a976ed1438b75682d8089ef14d0b9b2cc
Zak-Rey <zaktripple@gmail.com>
2023-04-12 20:21:12 +0300

    Inner class

--- Nested and Inner Class/CPU.java
+++ Nested and Inner Class/CPU.java
@@ -4,5 +4,18 @@ class CPU {
     class Processor{
         double cores;
         String manufacturer;
+
+        double getCache(){
+            return 4.3;
+        }
+    }
+
+    protected class RAM {
+        double memory;
+        String manufacturer;
+
+        double getClockspeed() {
+            return 3.2;
+        }
     }
 }
