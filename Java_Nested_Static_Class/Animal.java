752ff60969cc6282e33e01bf69dca4e975e1edc7
Zak-Rey <zaktripple@gmail.com>
2023-04-13 18:32:21 +0300

    java nested static class

---
+++ Java_Nested_Static_Class/Animal.java
@@ -0,0 +1,17 @@
+package stringIoStreams.Java_Nested_Static_Class;
+
+public class Animal {
+    // inner class
+   class Reptile {
+    public void displayInfo() {
+      System.out.println("I am a reptile.");
+    }
+ }
+
+    // static class
+   static class Mammal {
+    public void displayInfo() {
+      System.out.println("I am a mammal.");
+    }
+ }
+}
