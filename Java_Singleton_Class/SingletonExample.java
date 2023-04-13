f73bace0b3128cd97ce113904e967e2ea38c7ba6
Zak-Rey <zaktripple@gmail.com>
2023-04-13 19:22:39 +0300

    Java singleton class

--- Java_Singleton_Class/SingletonExample.java
+++ Java_Singleton_Class/SingletonExample.java
@@ -3,5 +3,8 @@ package Java_Singleton_Class;
 public class SingletonExample {
     //private field of Sinleton class type that refers to the object
     private SingletonExample singleObject;
+    //private constructor
+    private SingletonExample(){
 
+    }
 }
