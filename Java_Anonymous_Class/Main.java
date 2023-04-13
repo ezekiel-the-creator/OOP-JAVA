0c8c88bf87eb038b63d859660cd20b2045ef1d19
Zak-Rey <zaktripple@gmail.com>
2023-04-13 18:55:02 +0300

    Java anonymous class

---
+++ Java_Anonymous_Class/Main.java
@@ -0,0 +1,8 @@
+package Java_Anonymous_Class;
+
+public class Main {
+    public static void main(String... args){
+        Anonymous anony = new Anonymous();
+        anony.createClass();
+    }
+}
