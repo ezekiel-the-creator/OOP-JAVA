dcc9cf81e8976f8981f698b42e0e8c33da6d06f9
Zak-Rey <zaktripple@gmail.com>
2023-04-13 18:52:48 +0300

    Java anonymous class

---
+++ Java_Anonymous_Class/Anonymous.java
@@ -0,0 +1,11 @@
+package Java_Anonymous_Class;
+
+public class Anonymous {
+    public void createClass() {
+      Polygon p1 = new Polygon() {
+        public void display(){
+            System.out.println("Inside the anonymous class");
+        }
+        };
+    }
+}
